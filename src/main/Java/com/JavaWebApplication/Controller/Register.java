bf80186cd12e121a4bc8c2ba3cd3a88a9ca15ed6
ga0221 <52634933+ga0221@users.noreply.github.com>
2020-06-02 14:39:21 -0500

    Add files via upload

---
+++ src/main/Java/com/JavaWebApplication/Controller/Register.java
@@ -0,0 +1,62 @@
+package com.JavaWebApplication.Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.JavaWebApplication.Beans.RegisterBean;
+import com.JavaWebApplication.Model.UserDetails;
+
+/**
+ * Servlet implementation class Register
+ */
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Register() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("fullname");
+		String email = request.getParameter("emailId");
+		String password = request.getParameter("password");
+
+		RegisterBean regUser = new RegisterBean();
+		regUser.setName(name);
+		regUser.setEmail(email);
+		regUser.setPassword(password);
+
+		UserDetails user = new UserDetails();
+		String s1 = user.insertUserDetails(regUser);
+
+		out.println(s1);
+	}
+
+}
